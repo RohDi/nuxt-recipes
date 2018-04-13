909e64d598cbec4d61009e5e6c9231ed23ece253
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-04-13 10:36:13 +0900

    (C)reate pages/recipes/index.vue : recipes 하위 index 템플릿 작성

---
+++ pages/recipes/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="recipes">
+      <article class="recipe">
+          <div class="thumbnail" style="background-image: url('https://www.metro.ca/userfiles/image/recipes/pizza-saucisse-piquante-2301.jpg')"></div>
+          <h1>Title</h1>
+          <p>Some nice preview text</p>
+      </article>
+      <article class="recipe">
+          <div class="thumbnail" style="background-image: url('http://cdn0.wideopeneats.com/wp-content/uploads/2016/12/days-order-seafood-restaurant.png')"></div>
+          <h1>Title</h1>
+          <p>Some nice preview text</p>
+      </article>
+  </section>
+</template>
+
+<style scoped>
+
+.recipes {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+.recipe {
+    box-sizing: border-box;
+    width: 280px;
+    padding: 8px;
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 2px #aaa;
+    margin: 10px;
+}
+
+.thumbnail {
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+}
+
+
+</style>
