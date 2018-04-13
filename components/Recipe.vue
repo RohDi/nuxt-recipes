5ba5315c57d11cf5f40b66ce4f55159d748155e8
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-04-13 11:05:13 +0900

    (C)reate components/Recipe.vue : Recipe 컴포넌트 생성

---
+++ components/Recipe.vue
@@ -0,0 +1,34 @@
+<template>
+    <article class="recipe">
+        <div class="thumbnail" style="background-image: url('https://www.metro.ca/userfiles/image/recipes/pizza-saucisse-piquante-2301.jpg')"></div>
+        <h1>Title</h1>
+        <p>Some nice preview text</p>
+    </article>
+</template>
+
+<script>
+
+export default {
+}
+
+</script>
+
+<style scoped>
+
+.recipe {
+    box-sizing: border-box;
+    width: 280px;
+    padding: 8px;
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 2px #aaa;
+    margin: 10px;
+}
+
+.thumbnail {
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+}
+
+</style>
