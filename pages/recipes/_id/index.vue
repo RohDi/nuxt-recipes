a7edd5693fde9e9c71ab46f9e70d8278878f7001
Kim Yoon Sung <yoonsung071@gmail.com>
2018-04-23 01:41:21 +0900

    (U)pdate pages/recipes/_id/index.vue : Recipe 상세 정보를 보여주는 페이지 스타일링

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <p>{{ $route.params.id }}</p>
+  <section class="single-recipe">
+      <h1>TITLE</h1>
+      <div>
+          <img src="" alt="">
+      </div>
+      <p>The recipe decription.</p>
+  </section>
 </template>
+
+<style scoped>
+.single-recipe {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+</style>
