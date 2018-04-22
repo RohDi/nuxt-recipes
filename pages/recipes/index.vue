a70f50f2153374bb24d35d864d70d0ef4046622b
Kim Yoon Sung <yoonsung071@gmail.com>
2018-04-23 02:24:06 +0900

    (U)pdate pages/recipes/index.vue : ajax(Promise 함수)를 이용한 라우팅 테스트

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="recipes">
       <Recipe
-        thumbnail="https://bit.ly/2GVYWM2"
-        title="Delicious Pizza"
-        id="1"
-        previewText="Awesome Pizza"
+        :thumbnail="recipes[0].thumbnail"
+        :title="recipes[0].title"
+        :id="recipes[0].id"
+        :previewText="recipes[0].previewText"
       />
       <Recipe
         thumbnail="https://bit.ly/2qtNwou"
@@ -21,6 +21,28 @@ import Recipe from '@/components/Recipe'
 export default {
     components: {
         Recipe
+    },
+    asyncData(){
+        return new Promise((resolve, recejt) => {
+            setTimeout( () => {
+                resolve({
+                    recipes: [
+                        {
+                            id: "1",
+                            title: "Delicious Pizza(ajaxed)",
+                            previewText: "Awesome Pizza!",
+                            thumbnail: "https://bit.ly/2GVYWM2"
+                        },
+                        {
+                            id: "2",
+                            title: "Sea Food Salad(ajaxed)",
+                            previewText: "Very tasty indeed!",
+                            thumbnail: "https://bit.ly/2qtNwou"
+                        }
+                    ]
+                })
+            }, 1500)
+        })
     }
 }
 </script>
