f49ef63139b155ea463dd1d0cc55f1e0ee399b30
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-04-23 10:33:09 +0900

    (U)pdate pages/recipes/_id/index.vue : Recipe 상세 페이지에서 서버 사이드 데이터를 가져오도록 변경

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -1,12 +1,42 @@
 <template>
   <section class="single-recipe">
-      <h1>{{ $route.params.id }}</h1>
+      <h1>{{ recipe.title }}</h1>
       <div>
-          <img src="" alt="">
+          <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
       </div>
-      <p>The recipe decription.</p>
+      <p>{{ recipe.descriptionText }}</p>
   </section>
 </template>
+
+<script>
+export default {
+    asyncData(context){
+        return new Promise((resolve, recejt) => {
+            setTimeout( () => {
+                resolve({
+                    recipe: [
+                        {
+                            id: "1",
+                            title: "Delicious Pizza(ajaxed)",
+                            previewText: "Awesome Pizza!",
+                            descriptionText: "This is really a delicious meal!",
+                            thumbnail: "https://bit.ly/2GVYWM2"
+                        },
+                        {
+                            id: "2",
+                            title: "Sea Food Salad(ajaxed)",
+                            previewText: "Very tasty indeed!",
+                            descriptionText: "This is really a delicious meal!",
+                            thumbnail: "https://bit.ly/2qtNwou"
+                        }
+                    ].find(el => el.id === context.params.id)
+                })
+            }, 1500)
+        })
+    }
+}
+</script>
+
 
 <style scoped>
 .single-recipe {
@@ -16,5 +46,11 @@
     align-items: center;
     text-align: center;
     padding: 30px;
+}
+
+.recipe-image {
+    width: 500px;
+    height: 500px;
+
 }
 </style>
